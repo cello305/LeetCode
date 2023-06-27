6354aa47a145ff4fcdecf395770b7461308559f6
Emanuel Warzel <44512700+cello305@users.noreply.github.com>
2023-06-27 19:10:51 -0400

    Brute force method original

--- TwoSum/TwoSumBruteForce.java
+++ TwoSum/TwoSumBruteForce.java
@@ -1,17 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
-        Map<Integer, Integer> complements = new HashMap<>(); //hashmap initalize
-
-         for(int i = 0; i < nums.length; i++){
-             Integer complementIndex = complements.get(nums[i]);
-            if(complementIndex != null){
-                return new int[]{i, complementIndex};
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i, j};
+                }
             }
-            complements.put(target - nums[i], i);
-            
         }
-    
-    return nums;
-}
+        return new int[0];
+    }
 }
